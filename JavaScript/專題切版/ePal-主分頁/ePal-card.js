10fcb82747fb37353ff1b262877e8a91ad9d4444
唐樂維 <le.kentang@gmail.com>
2021-09-02 15:06:39 +0800

    ePal-recommend

---
+++ JavaScript/專題切版/ePal-主分頁/ePal-card.js
@@ -0,0 +1,26 @@
+let VoiceBtn = document.querySelector('.user-Voice')
+let spanOdd = document.querySelectorAll('.voice-Left span:nth-child(odd)')
+let spanEven = document.querySelectorAll('.voice-Left span:nth-child(even)')
+console.log(spanEven[0])
+spanOdd.forEach(item => {
+    VoiceBtn.addEventListener('click', () => {
+        setInterval(() => {
+            item.classList.toggle('w-15')
+            item.classList.toggle('w-10')
+        }, 400)
+    })
+})
+spanEven.forEach(item => {
+    VoiceBtn.addEventListener('click', () => {
+        setInterval(() => {
+            item.classList.toggle('w-10')
+            item.classList.toggle('w-15')
+        }, 400)
+    })
+})
+var left = document.querySelector(".left");
+let navBar = document.querySelector('.top_navbar');
+navBar.addEventListener("click", function() {
+    document.querySelector("body").classList.toggle("active");
+    left.classList.toggle('turn-right');
+})
